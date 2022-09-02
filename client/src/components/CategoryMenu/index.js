680ff0579403e92d31b774535440f83fcf2f7e9c
Amanda Klenk <amandamklenk3@gmail.com>
2022-09-02 12:51:08 -0400

    set up context in CategoryMenu component instead of props

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
+import { useStoreContext } from '../../utils/GlobalState';
 
-function CategoryMenu({ setCategory }) {
+function CategoryMenu() {
+  // this is replaced with useStoreContext
+  // const { data: categoryData } = useQuery(QUERY_CATEGORIES);
+  // const categories = categoryData?.categories || [];
+
+  const [state, dispatch] = useStoreContext();
+
+  const { categories } = state;
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+
+  useEffect(() => {
+    // if categoryData exists or has changed from the response of the query, then run dispatch
+    if (categoryData) {
+      // execute dispatch function with action object indicating type of action and data
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+    }
+  }, [categoryData, dispatch]);
+
+  const handleClick = id => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
+    });
+  };
 
   return (
     <div>
@@ -13,7 +39,7 @@ function CategoryMenu({ setCategory }) {
         <button
           key={item._id}
           onClick={() => {
-            setCategory(item._id);
+            handleClick(item._id);
           }}
         >
           {item.name}
