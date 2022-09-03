499ef9b12e337b371ae90c5fed18b71321fdee4a
Amanda Klenk <amandamklenk3@gmail.com>
2022-09-03 11:08:19 -0400

    make cart funcitonal with remove and update

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -9,9 +9,20 @@ const Cart = () => {
     // set up useStoreContext state
     const [state, dispatch] = useStoreContext();
 
+    console.log(state);
+
     // function to toggle the cart to open or close
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
+    };
+
+    // add up the prices of everything in state.cart
+    function calculateTotal() {
+        let sum = 0;
+        state.cart.forEach(item => {
+            sum += item.price * item.purchaseQuantity;
+        });
+        return sum.toFixed(2);
     }
 
     // this will toggle, clicking this will open and give expanded cart
@@ -24,26 +35,35 @@ const Cart = () => {
     }
 
     return (
-        <div className='cart'>
-            <div className='close' onClick={toggleCart}>[close]</div>
-            <h2>Shopping Cart</h2>
+    <div className="cart">
+        <div className="close" onClick={toggleCart}>[close]</div>
+        <h2>Shopping Cart</h2>
+        {state.cart.length ? (
             <div>
-                <CartItem item={{name:'Camera', image:'camera.jpg', price:5, purchaseQuantity:3}} />
-                <CartItem item={{name:'Soap', image:'soap.jpg', price:6, purchaseQuantity:4}} />
-
-                <div className='flex-row space-between'>
-                    <strong>Total: $0</strong>
-                    { Auth.loggedIn() ?
+                {state.cart.map(item => (
+                <CartItem key={item._id} item={item} />
+                ))}
+                <div className="flex-row space-between">
+                    <strong>Total: ${calculateTotal()}</strong>
+                    {
+                        Auth.loggedIn() ?
                         <button>
                             Checkout
                         </button>
-                        : 
+                        :
                         <span>(log in to check out)</span>
                     }
                 </div>
             </div>
-        </div>
-    );
-};
+            ) : (
+                <h3>
+                    <span role="img" aria-label="shocked">
+                        😱
+                    </span>
+                    You haven't added anything to your cart yet!
+                </h3>
+            )}
+    </div>
+    )};
 
 export default Cart;
